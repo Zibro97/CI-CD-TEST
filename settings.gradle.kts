93a315127e1f65797e297555807575cac0cadc6c
Zibro97 <jgh6272@naver.com>
2024-01-26 14:59:05 +0900

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CiCdExample"
+include(":app")
+ 
